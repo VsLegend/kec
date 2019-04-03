5b88196c821b0b56a1894c170ace9f9543c90333
Wangjunwei <291123959@qq.com>
2019-04-03 16:30:39 +0800

    用户首页热门主贴展示

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -2,6 +2,25 @@
   $(function(){
 
     $('.sidenav').sidenav();
-
+    //输入框的字数限制
+    $('input#post_name, textarea#textarea2').characterCounter();
+    //小贴士工具
+    $('.tooltipped').tooltip();
+    //Select jQuery
+    $('select').formSelect();
+    //collapsibel
+    $('.collapsible').collapsible();
+    //对话框初始化
+    $('.modal').modal();
+    //传送带初始化
+    $('.carousel').carousel();
+    //右侧固定导航
+    $('.scrollspy').scrollSpy();
+    //新闻
+    $('.carousel.carousel-slider').carousel({
+      //宽度是否为全屏宽度
+      fullWidth: false,
+      indicators: true
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
